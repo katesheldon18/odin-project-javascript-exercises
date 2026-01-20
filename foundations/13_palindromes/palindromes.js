3a74d1febd89eae9eb3565946413980f80d65e86
Kate <katshe18@gmail.com>
2026-01-20 15:54:10 +1300

    /lessons/foundations-object-basics

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const input = 'A car, a man, a maraca.'
 
+const palindromes = function (input) {
+    const plainString = input.toLowerCase().split('').filter((item) => (item !== ',' && item !== '.' && item !== ' ')).join('');
+    let reversedLetters = [...plainString].reverse().join('');
+    return plainString === reversedLetters;
 };
+
+console.log(palindromes(input));
+console.log(palindromes('kate test'));
 
 // Do not edit below this line
 module.exports = palindromes;
--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,44 @@
-const findTheOldest = function() {
+const people = [
+    {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+    },
+    {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+    },
+    {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+    },
+    {
+    name: "Kent",
+    yearOfBirth: 1912,
+    },
+]
 
+const findTheOldest = function(people) {
+    let currentOldest = people[0];
+
+    for (let i = 0; i < people.length; i++) {
+        let person = people[i];
+        if (person.yearOfDeath) {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        } else if (!person.yearOfDeath) {
+            person.age = new Date().getFullYear() - person.yearOfBirth;
+        }
+        if (person.age > currentOldest.age) {
+            currentOldest = person;
+        }
+    }
+
+    return currentOldest;
 };
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
